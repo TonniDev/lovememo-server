b5808672a17ddaf0c8f42506f1a31572a3523507
Tonni Andrade <tonni.andrade@gmail.com>
2024-09-23 09:54:38 -0300

    [UPDATE] User create DTO with new columns

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -4,4 +4,7 @@ export class CreateUserDto {
   public email: string;
   public password?: string;
   public oAuthId?: string;
+  public oauth_id?: string;
+  public oauth_providers?: string[];
+  public oauth_info?: Record<string, any>;
 }
